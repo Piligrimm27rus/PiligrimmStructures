794a4047113ab7528d1e6a3db3b2b6e17098ae3e
Piligrimm27rus <cod400446@gmail.com>
2024-04-11 23:03:59 +0500

    first

---
+++ Structures/Stack/Stack.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+
+namespace Piligrimm;
+
+public class Stack<T> : IEnumerable<T>, ICollection
+{
+    private int _count = 0;
+    private Node? currentNode;
+
+    public int Count => _count;
+    public bool IsSynchronized => throw new NotImplementedException();
+    public object SyncRoot => throw new NotImplementedException();
+
+    public Stack() { }
+
+    public Stack(params T[] values)
+    {
+        foreach (var item in values)
+        {
+            Push(item);
+        }
+    }
+
+    public void CopyTo(Array array, int index)
+    {
+        array.
+        Node? _currentNode = null;
+        for (int i = 0; i < _count; i++)
+        {
+            _currentNode = _currentNode is null ? currentNode : _currentNode.Previous;
+            array[index + i] = _currentNode.Data;
+        }
+    }
+
+    public void Push(T item)
+    {
+        currentNode = new Node(item, currentNode);
+        _count++;
+    }
+
+    public T Pop()
+    {
+        if (_count == 0 || currentNode is null)
+            throw new InvalidOperationException("Stack is empty");
+
+        T data = currentNode.Data;
+        currentNode = currentNode?.Previous;
+        _count--;
+        // if (currentNode.Previous is null)
+        //     currentNode = null;
+        // else currentNode = currentNode.Previous;
+
+
+        return data;
+    }
+
+    public T Peek()
+    {
+        if (_count == 0 || currentNode is null)
+            throw new InvalidOperationException("Stack is empty");
+
+        return currentNode.Data;
+    }
+
+    public void Clear()
+    {
+        _count = 0;
+        currentNode = null;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        Node? _currentNode = null;
+        for (int i = 0; i < _count; i++)
+        {
+            _currentNode = _currentNode is null ? currentNode : _currentNode.Previous;
+
+            yield return _currentNode.Data;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        yield return currentNode is null ? default : currentNode.Data;
+    }
+
+    private class Node
+    {
+        public T Data { get; set; }
+        public Node? Previous { get; set; }
+
+        public Node(T item, Node? node = null)
+        {
+            Data = item;
+            Previous = node;
+        }
+    }
+}
+
---
+++ Tests/StackTests/StackTests.cs
@@ -0,0 +1,11 @@
+namespace Piligrimm.Stask.Tests;
+
+public class StaskTests
+{
+    [Fact]
+    private void Push()
+    {
+        var stack = new Stack<string>();
+        // stack.
+    }
+}
