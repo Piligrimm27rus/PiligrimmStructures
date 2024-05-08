cf3320601ea2933d91793e5bf18ca000ee086a83
Piligrimm27rus <cod400446@gmail.com>
2024-05-08 21:22:57 +0500

    first Dictionary

---
+++ Structures/Dictionary/Dictionary.cs
@@ -0,0 +1,12 @@
+namespace Piligrimm.Structures;
+
+public class Dictionary<K, V>
+{
+    private T[] _array;
+    private int _capacity = 5; // 5 is default capacity
+    private int _count = 0;
+
+    public int Count => _count;
+
+    Add
+}
